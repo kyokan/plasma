ce8485453d88f0e8b7dae2b94d669d6895fc67c7
gcristea <gcristea@uw.edu>
2018-09-26 13:02:14 -0700

    Testing (#76)
    * Added js library for integration testing through gRPC. Running ganache in deterministic mode (same account and contract addresses for every run.)
    
    * Added call to deposit from the integration tests. Removed database/storage initialization from function (initHandler) called by handlers for Deposit, Exit and Finalize.
    
    * Moved tests around.
    
    * Improved tests.
    
    * Refactored tests.

--- rpc-test-client/test/deposit.js
+++ rpc-test-client/test/deposit.js
@@ -1,28 +1,13 @@
-const W3 = require("web3");
-const _ = require('lodash');
+const _  = require('lodash');
+const b4 = require('./init');
 const expect = require('chai').expect;
-const plasma = require('../index');
 
-const plasmaJson = require('../../contracts/build/contracts/Plasma');
-const contractItf = plasmaJson['abi'];
-const web3 = new W3(`http://localhost:9545`);
-
-// These should be the same across runs as ganache-cli is ran deterministic, bin/start
-const accountAddress  = '0x627306090abab3a6e1400e9345bc60c78a8bef57';
-const accountKey      = 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3';
-const contractAddress = '0xf25186b5081ff5ce73482ad761db0eb0d25abfbf';
-let contract;
-let accounts;
-let client;
-
-before(async () => {
-    contract = new web3.eth.Contract(contractItf, contractAddress);
-    client = new plasma.PlasmaClient(__dirname + './../../rpc/proto/root.proto', 'localhost:8643');
-});
+let accounts = b4.Accounts;
+let web3     = b4.Web3;
 
 describe('Deposit (long test)', () => {
     it('deposits funds from first account and checks balance', function (done) {
-        let account = new plasma.Account(client, web3, contract, accountAddress, accountKey);
+        let account = accounts[0];
         const amount = web3.utils.toBN(web3.utils.toWei('1', 'ether'));
         account.GetPlasmaBalance((err, initialBalance) => {
             expect(err).to.equal(null);
