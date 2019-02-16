f312b779e0261b854c2c59f234e58400f143b459
Matthew Slipper <me@matthewslipper.com>
2019-02-15 23:36:32 -0800

    Merge DB helpers

--- db/helpers.go
+++ db/helpers.go
@@ -5,7 +5,10 @@ import (
 	"strconv"
 		"github.com/ethereum/go-ethereum/common"
 	"github.com/kyokan/plasma/chain"
-		)
+	"github.com/kyokan/plasma/util"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"strings"
+)
 
 const merkleKeyPrefix = "merkle"
 const blockKeyPrefix = "blk"
@@ -39,4 +42,61 @@ func blockFeesKey(number uint64) []byte {
 
 func extractAmount(tx *chain.ConfirmedTransaction, addr common.Address) *big.Int {
 	return tx.Transaction.Body.OutputFor(&addr).Amount
+}
+
+const (
+	txPrefix      = "tx"
+	spendPrefix   = "spend"
+	utxoPrefix    = "utxo"
+	depositPrefix = "deposit"
+)
+
+func txByHashKey(hash string) []byte {
+	return joinKey(txPrefix, "hash", hash)
+}
+
+func txByBlockNumTxIdxKey(blockNum uint64, txIdx uint32) []byte {
+	return joinKey(txPrefix, "blockNumTxIdx", util.Uint642Str(blockNum), util.Uint322Str(txIdx))
+}
+
+func txByBlockNumIterKey(blockNum uint64) []byte {
+	return joinKey(txPrefix, "blockNumTxIdx", util.Uint642Str(blockNum))
+}
+
+func utxoKey(addr common.Address, hash util.Hash, outIdx uint8) []byte {
+	return joinKey(utxoPrefix, hexutil.Encode(addr[:]), hexutil.Encode(hash), util.Uint82Str(outIdx))
+}
+
+func utxoAddrIterKey(addr common.Address) []byte {
+	return joinKey(utxoPrefix, hexutil.Encode(addr[:]))
+}
+
+func depositKey(nonce *big.Int) []byte {
+	return joinKey(depositPrefix, util.Big2Str(nonce))
+}
+
+func utxoKeyParts(key string) (util.Hash, uint8) {
+	parts := strings.Split(key, keyPartsSeparator)
+	if len(parts) != 4 {
+		panic("invalid UTXO key")
+	}
+	hash, err := hexutil.Decode(parts[2])
+	if err != nil {
+		panic("invalid tx hash")
+	}
+	outIdx, ok := util.Str2Uint8(parts[3])
+	if !ok {
+		panic("invalid output index")
+	}
+	return hash, outIdx
+}
+
+func spendByTxIdxKey(blockNum uint64, txIdx uint32, outIdx uint8) []byte {
+	return joinKey(
+		spendPrefix,
+		"blockNumTxIdx",
+		util.Uint642Str(blockNum),
+		util.Uint322Str(txIdx),
+		util.Uint82Str(outIdx),
+	)
 }
