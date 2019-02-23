ea8ddbc5a356c38cd12ef1997290472cfd22fad7
Matthew Slipper <me@matthewslipper.com>
2019-02-23 11:51:53 -0800

    Add startExit support in Plasma client

--- kyokan-plasma-client/src/Plasma.ts
+++ kyokan-plasma-client/src/Plasma.ts
@@ -12,6 +12,8 @@ import {toBig} from './util/numbers';
 import {OnChainDeposit} from './domain/OnChainDeposit';
 import IRootClient from './rpc/IRootClient';
 import {RESTRootClient} from './rpc/RESTRootClient';
+import Outpoint from './domain/Outpoint';
+import ExitOperation from './operations/ExitOperation';
 
 /**
  * Options used to construct a new Plasma client instance.
@@ -154,6 +156,51 @@ export default class Plasma {
   public async deposit (value: NumberLike): Promise<OnChainDeposit> {
     this.ensureKey();
     return this.contract().deposit(toBig(value), this.fromAddress!);
+  }
+
+  /**
+   * Starts exiting the provided value or outpoint. If a value is provided,
+   * the outpoint whose amount is closest but higher than the given value is
+   * chosen. For example, if you have two UTXOs with amount 1.5ETH and 2ETH
+   * and you call startExit with 1ETH, the 1.5ETH outpoint will be exited.
+   *
+   * The bond parameter is used to to protect against double-spends. Your bond
+   * must be higher than the value of your outpoint. The value of the bond will
+   * be returned when the challenge period expires.
+   *
+   * WARNING: You will forfeit your bond if you attempt to exit a spent outpoint.
+   *
+   * @param valueOrOutpoint The or outpoint to exit.
+   * @param bond The bond to post guaranteeing this exit.
+   */
+  public async startExit(valueOrOutpoint: NumberLike|Outpoint, bond: NumberLike): Promise<void> {
+    this.ensureKey();
+    let outpoint: Outpoint|null = null;
+
+    if (valueOrOutpoint instanceof Outpoint) {
+      outpoint = valueOrOutpoint;
+    } else {
+      const value = toBig(valueOrOutpoint);
+      const utxos = await this._rootClient.getUTXOs(this.fromAddress!);
+      // outpoints are in descending-value order
+      for (let i = utxos.length - 1; i >= 0; i--) {
+        const utxo = utxos[i];
+        if (utxo.amount.gte(value)) {
+          outpoint = utxo;
+          break;
+        }
+      }
+    }
+
+    if (!outpoint) {
+      throw new Error('no suitable UTXOs')
+    }
+
+    const exitOp = new ExitOperation(this._contract, this._rootClient, this.fromAddress!)
+      .withOutpoint(outpoint)
+      .withCommittedFee(toBig(bond));
+
+    await exitOp.exit();
   }
 
   private ensureKey () {
