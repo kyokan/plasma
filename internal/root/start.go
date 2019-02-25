13548701295e4c7f4a102446da56755ae42b5ddf
kirushanths <kirushanth.2@gmail.com>
2019-02-25 20:46:47 +0000

    no new line

--- internal/root/start.go
+++ internal/root/start.go
@@ -70,6 +70,6 @@ func Start(config *config.GlobalConfig, privateKey *ecdsa.PrivateKey) error {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
-	fmt.Println("Received an interrupt, stopping services...\n")
+	fmt.Println("Received an interrupt, stopping services...")
 	return nil
 }
